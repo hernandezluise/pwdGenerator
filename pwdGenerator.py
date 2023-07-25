52d51ba94a2941d4831ce94d51d5fd32b044d052
Luis Hernandez <hernandezluise@protonmail.com>
2023-07-24 17:09:51 -0700

    pwdGenerator file

---
+++ pwdGenerator.py
@@ -0,0 +1,16 @@
+import random
+import string
+
+def pwdGen(num_of_letters, num_of_digits, num_of_special_characters):
+    letters = ''.join((random.choice(string.ascii_letters) for i in range(num_of_letters)))
+    digits = ''.join((random.choice(string.digits) for i in range (num_of_digits)))
+    characters = ''.join((random.choice(string.punctuation) for i in range (num_of_special_characters)))
+
+    pwd_list = list(letters + digits + characters)
+    random.shuffle(pwd_list)
+
+    pwd = ''.join(pwd_list)
+    print("Random Password: ", pwd)
+
+# enter number of letters, numbers, special characters to generate password of desired length
+pwdGen(20, 3, 4)
